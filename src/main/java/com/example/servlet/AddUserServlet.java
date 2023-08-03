2ea1bdd9b396ac94d8e0081df11fb5c3f253eabf
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-08-03 14:49:38 +0400

    aa

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,31 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Navigate to the /add page
+        request.getRequestDispatcher("/add.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+
+
+        request.setAttribute("user", firstName + " " + lastName);
+
+        request.getRequestDispatcher("/add.jsp").forward(request, response);
+    }
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,24 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
-    //write your code here!
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/users")
+public class GetUsersServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<String> users = List.of("John Doe", "Jane Smith", "Michael Johnson");
+
+        request.setAttribute("users", users);
+
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
+    }
 }
